70f83c1778a5b0603d9833db99edf7980f9b23dd
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-10-16 17:37:50 +0000

    update

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,19 +1,18 @@
 function calculate(weight, height) {
     let height_n = (height/100)**2;
-    let result_cal = weight/height_n;
+    let result = weight/height_n;
     if (result < 18.5) {
-        result = "Your result is " + result_cal + "\nUnderweight";
-      } else if (bmi < 25) {
-        result = "Your result is " + result_cal + "\nNormal weight";
-      } else if (bmi < 30) {
-        result = "Your result is " + result_cal + "\nOverweight (Obesity Class 1)";
-      } else if (bmi < 35) {
-        result = "Your result is " + result_cal + "\nOverweight (Obesity Class 2)";
-      } else if (bmi < 40) {
-        result = "Your result is " + result_cal + "\nOverweight (Obesity Class 3, Severe Obesity)";
-      } else {
-        result = "Your result is " + result_cal + "\nOverweight (Obesity Class 4, Very Severe Obesity)";
-      }
-    return result;
+        result_text = "Underweight";
+        } else if (bmi < 25) {
+        result_text = "Normal weight";
+        } else if (bmi < 30) {
+        result_text = "Overweight (Obesity Class 1)";
+        } else if (bmi < 35) {
+        result_text = "Overweight (Obesity Class 2)";
+        } else if (bmi < 40) {
+        result_text = "Overweight (Obesity Class 3, Severe Obesity)";
+        } else {
+        result_text = "Overweight (Obesity Class 4, Very Severe Obesity)";
+        }
+    return [result, result_text];
 }
-// I will fix this code in the future
