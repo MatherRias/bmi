ba513550bc5e0ac7ce5d6d2a730087bc3d808f26
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-10-16 10:06:16 +0000

    update

---
+++ docs/script/script.js
@@ -0,0 +1,18 @@
+function calculate(weight, height) {
+    let height_n = (height/100)**2;
+    let result = weight/height_n;
+    if (result < 18.5) {
+        result_text = "Underweight";
+      } else if (bmi < 25) {
+        result_text = "Normal weight";
+      } else if (bmi < 30) {
+        result_text = "Overweight (Obesity Class 1)";
+      } else if (bmi < 35) {
+        result_text = "Overweight (Obesity Class 2)";
+      } else if (bmi < 40) {
+        result_text = "Overweight (Obesity Class 3, Severe Obesity)";
+      } else {
+        result_text = "Overweight (Obesity Class 4, Very Severe Obesity)";
+      }
+    return result;
+}
